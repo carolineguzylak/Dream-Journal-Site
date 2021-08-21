e1901cfa7f6eb6ab10d4fab28792973c1921eeb1
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2021-08-20 23:02:25 -0700

    Can now add dream types/emotions to description of each dream

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
+var dreamTypesID = ["#happy", "#stressful", "#sad", "#boring", "#exciting", "#nightmare"]
+var dreamTypes = ["happy", "stressful", "sad", "boring", "exciting", "nightmare"]
+
 $("#submitDream").click(function(){
     var desc = $("#dreamDescription").val();
-    $("#dreamList").append("<li>" + desc + "</li>");
+    var types = " "
+
+    for (var i=0; i<dreamTypesID.length; i++){
+        var type = $(dreamTypesID[i]).is(":checked");
+        if (type == true){
+            types += dreamTypes[i] + " ";
+        }
+    }
+
+    $("#dreamList").append("<li>" + desc + types + "</li>");
 });
