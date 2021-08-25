203caba8ae94216ce68fa8164bd3ab656da2a8ef
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2021-08-24 22:37:37 -0700

    Added navbar and new dream display. Only one dream will be displayed at a time, and dreams can be flipped through using previous and next dream buttons.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
-var dreamTypesID = ["#happy", "#stressful", "#sad", "#boring", "#exciting", "#nightmare"]
-var dreamTypes = ["happy", "stressful", "sad", "boring", "exciting", "nightmare"]
+const dreamTypesID = ["#happy", "#stressful", "#sad", "#boring", "#exciting", "#nightmare"];
+const dreamTypes = ["happy", "stressful", "sad", "boring", "exciting", "nightmare"];
+
+// dreams the user has provided so far
+var addedDreams = [];
+var dreamIndex = -1;
+
+
 
 class Dream{
     constructor(desc, types){
@@ -14,23 +20,54 @@ class Dream{
 
 }
 
-$("#submitDream").click(function(){
+function displayDream(d){
+    // this is called a template literal
+    // $("#dreamList").append(`<li> ${d.to_str()} </li>`);
 
-    var types = " "
+    // this removes previous dream from the display
+    $("#dreamDescBox").find("p").remove()
+    $("#typeBox").find("p").remove()
+
+    $("#dreamDescBox").append(`<p>${d.desc}</p>`);
+    $("#typeBox").append(`<p>${d.types}</p>`);
+}
+
+$("#submitDream").click(function(){
+    var types = []
     for (var i=0; i<dreamTypesID.length; i++){
         var type = $(dreamTypesID[i]).is(":checked");
         if (type == true){
-            types += dreamTypes[i] + " ";
+            types.push(dreamTypes[i]);
         }
         // unchecks all boxes
         $(dreamTypesID[i]).prop('checked', false);
     }
 
-    let d = new Dream($("#dreamDescription").val(), types)
-
-    $("#dreamList").append("<li>" + d.to_str() + "</li>");
+    let d = new Dream($("#dreamDescription").val(), types);
+    addedDreams.push(d)
+    dreamIndex += 1;
+    console.log(dreamIndex)
 
     // sets description to empty
     $("#dreamDescription").val("");
 
+    displayDream(d);
+});
+
+$("#nextDream").click(function(){
+    if (dreamIndex < addedDreams.length - 1){
+        dreamIndex += 1;
+        displayDream(addedDreams[dreamIndex])
+    }
+    console.log(dreamIndex)
+    console.log(addedDreams)
+});
+
+$("#prevDream").click(function(){
+    if (dreamIndex > 0){
+        dreamIndex -= 1;
+        displayDream(addedDreams[dreamIndex])
+    }
+    console.log(dreamIndex)
+    console.log(addedDreams)
 });
