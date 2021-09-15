65cd9dbd0cc3a4381e44b730fa7d7233bf921aa2
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2021-09-14 21:18:26 -0700

    created PORT in express,js, added engines to package.json

--- express.js
+++ express.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app = express();
+
+// the '/' is if we are at localhost:5000/ 
+// we are sending a get request to the root route
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+
+
+// this dynamically tells us which port we need to 
+// be listening to according to Heroku
+// BUT if a port hasn't been defined by Heroku,
+// use 5000 as we are in a development environment
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
