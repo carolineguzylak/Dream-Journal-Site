44e50ac70361534d8e8c304050a643fcf1143eb0
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2021-09-16 13:25:19 -0700

    Changed express.js to use res.sendFile method in app.get rather than express.static in app.use

--- express.js
+++ express.js
@@ -7,14 +7,20 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 
-app.use(express.static('client'))
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+// THIS OPENS THE STATIC WEBPAGE
+// has the same function as res.sendFile('client/index.html')
+// in app.get
+// app.use(express.static('client'))
 
 // the '/' is if we are at localhost:5000/ 
 // we are sending a get request to the root route
-// app.get('/', (req, res) => {
-//     // res.send({bye: 'see you soon!'});
-//     res.render('index.html');
-// });
+app.get('/', (req, res) => {
+    res.sendFile('client/index.html')
+});
 
 
 
