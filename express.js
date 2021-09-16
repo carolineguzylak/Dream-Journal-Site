805dfe335f27ee4995c9c6865840b4012df2e68a
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2021-09-16 13:02:18 -0700

    can now open static site by running node express.js.

--- express.js
+++ express.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const app = express();
-
-// the '/' is if we are at localhost:5000/ 
-// we are sending a get request to the root route
-app.get('/', (req, res) => {
-    res.send({bye: 'see you soon!'});
-});
-
-
-
 // this dynamically tells us which port we need to 
 // be listening to according to Heroku
 // BUT if a port hasn't been defined by Heroku,
 // use 5000 as we are in a development environment
 const PORT = process.env.PORT || 5000;
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('client'))
+
+// the '/' is if we are at localhost:5000/ 
+// we are sending a get request to the root route
+// app.get('/', (req, res) => {
+//     // res.send({bye: 'see you soon!'});
+//     res.render('index.html');
+// });
+
+
+
 
 app.listen(PORT);
